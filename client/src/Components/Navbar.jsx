3ef090ac502c32d80a4ad370b48c1094df4a4f61
Abdumalik Shukrilloev <shamf120804@gmail.com>
2026-03-10 01:36:49 +0100

    updateCart with DB

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <div className="hidden sm:flex items-center gap-8">
         <NavLink to={"/"}>Home</NavLink>
-        <NavLink to={"/"}>Products</NavLink>
+        <NavLink to={"/products"}>Products</NavLink>
         <NavLink to={"/"}>Contacts</NavLink>
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
--- client/src/Context/AppContext.jsx
+++ client/src/Context/AppContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
 import axios from "axios";
 
@@ -38,7 +37,7 @@ export const AppContextProvider = ({ children }) => {
   // Fetch User Auth Status, User Data and Cart Items
   const fetchUser = async () => {
     try {
-      const { data } = await axios.get("api/user/is-auth");
+      const { data } = await axios.get("/api/user/is-auth");
       if (data.success) {
         setUser(data.user);
         setCartItems(data.user.cartItems);
@@ -124,6 +123,23 @@ export const AppContextProvider = ({ children }) => {
     fetchProducts();
     fetchSeller();
   }, []);
+
+  // Update Database Cart Items
+  useEffect(() => {
+    const updateCart = async () => {
+      try {
+        const { data } = await axios.post("/api/cart/update", { cartItems });
+        if (!data.success) {
+          toast.error(data.message);
+        }
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+    if (user) {
+      updateCart();
+    }
+  }, [cartItems]);
   const value = {
     navigate,
     user,
